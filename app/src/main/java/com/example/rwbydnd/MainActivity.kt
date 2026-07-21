6348ab7e5248ae1a5886c5e9bb9cad4093e96f8d
pinkcustard2 <zachthakur@outlook.com>
2026-07-21 15:52:20 +0100

    Added rules page

--- app/src/main/java/com/example/rwbydnd/MainActivity.kt
+++ app/src/main/java/com/example/rwbydnd/MainActivity.kt
@@ -6,20 +6,27 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowRight
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowRight
 import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Star
@@ -36,6 +43,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -43,6 +51,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -104,37 +113,148 @@ class MainActivity : ComponentActivity() {
                             items(sortedCharacters, key = { it.characterId }) { character ->
                                 var isToggled = favourites[character.characterId] ?: false
 
-                                Row(modifier = Modifier.fillMaxSize().animateItem(), verticalAlignment = Alignment.CenterVertically)
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .animateItem(),
+                                    verticalAlignment = Alignment.CenterVertically
+                                )
                                 {
                                     IconButton(
-                                        onClick = { favourites[character.characterId] = !isToggled
-                                            println("CLICKED ${character.characterId}")}
+                                        onClick = {
+                                            favourites[character.characterId] = !isToggled
+                                            println("CLICKED ${character.characterId}")
+                                        }
                                     ) {
                                         Icon(
-                                            imageVector = if (isToggled) {Icons.Filled.Star} else {Icons.Outlined.StarRate},
+                                            imageVector = if (isToggled) {
+                                                Icons.Filled.Star
+                                            } else {
+                                                Icons.Outlined.StarRate
+                                            },
                                             contentDescription = if (isToggled) "Selected icon button" else "Unselected icon button."
                                         )
                                     }
                                     Box(
                                         modifier = Modifier
                                             .fillMaxHeight()
-                                            .padding(15.dp, 5.dp)
+                                            .padding(10.dp, 5.dp)
                                             .weight(1f),
                                     ) { Text(character.characterName, fontSize = 18.sp) }
                                     Icon(
                                         imageVector = Icons.Filled.ChevronRight,
                                         contentDescription = "View character",
-                                        Modifier.padding(0.dp,0.dp, 10.dp, 0.dp)
+                                        Modifier.padding(0.dp, 0.dp, 10.dp, 0.dp)
                                     )
                                 }
                             }
                         }
-                    }
-                    FloatingActionButton(
-                        onClick = {  },
-                        modifier = Modifier.padding(innerPadding).padding(310.dp, 710.dp, 0.dp, 0.dp)
-                    ) {
-                        Icon(Icons.Filled.Add, "Floating action button.")
+                        FloatingActionButton(
+                            onClick = {  },
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .padding(310.dp, 710.dp, 0.dp, 0.dp)
+                        ) {
+                            Icon(Icons.Filled.Add, "Floating action button.")
+                        }
+                    } else
+                    {
+                        val rules = remember{ mutableStateListOf(
+                            false, false
+                        )}
+                        Box(modifier = Modifier
+                            .padding(10.dp, 85.dp, 10.dp, 25.dp)
+                            .clip(RoundedCornerShape(16.dp))
+                            .background(MaterialTheme.colorScheme.surfaceContainer)
+                            .fillMaxWidth()
+                        )
+                        {
+                            LazyColumn(Modifier.padding(10.dp))
+                            {
+                                item{
+                                    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.clickable {
+                                        rules[0] = !rules[0]
+                                    })
+                                    {
+                                        Box(modifier = Modifier
+                                            .fillMaxWidth()
+                                            .weight(1f)
+                                            .padding(5.dp, 5.dp))
+                                        {
+                                            Text(rules[0].toString(), fontSize = 18.sp)
+                                        }
+                                        if(rules[0])
+                                        {
+                                            Icon(
+                                                imageVector = Icons.Filled.ArrowDropDown,
+                                                contentDescription = "Expanded",
+                                            )
+                                        }
+                                        else
+                                        {
+                                            Icon(
+                                                imageVector = Icons.AutoMirrored.Filled.ArrowRight,
+                                                contentDescription = "Expanded",
+                                            )
+                                        }
+                                    }
+                                }
+                                if(rules[0])
+                                {
+                                    item{
+                                        Box(modifier = Modifier
+                                            .padding(5.dp)
+                                            .clip(RoundedCornerShape(16.dp))
+                                            .background(MaterialTheme.colorScheme.primaryContainer)
+                                            .fillMaxWidth())
+                                        {
+                                            Text("asdfsdafsdafssadfs", Modifier.padding(10.dp, 5.dp))
+                                        }
+                                    }
+                                }
+                                item{
+                                    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.clickable {
+                                        rules[1] = !rules[1]
+                                    })
+                                    {
+                                        Box(modifier = Modifier
+                                            .fillMaxWidth()
+                                            .weight(1f)
+                                            .padding(5.dp, 5.dp))
+                                        {
+                                            Text(rules[1].toString(), fontSize = 18.sp)
+                                        }
+                                        if(rules[1])
+                                        {
+                                            Icon(
+                                                imageVector = Icons.Filled.ArrowDropDown,
+                                                contentDescription = "Expanded",
+                                            )
+                                        }
+                                        else
+                                        {
+                                            Icon(
+                                                imageVector = Icons.AutoMirrored.Filled.ArrowRight,
+                                                contentDescription = "Expanded",
+                                            )
+                                        }
+                                    }
+                                }
+                                if(rules[1])
+                                {
+                                    item{
+                                        Box(modifier = Modifier
+                                            .padding(5.dp)
+                                            .clip(RoundedCornerShape(16.dp))
+                                            .background(MaterialTheme.colorScheme.primaryContainer)
+                                            .fillMaxWidth())
+                                        {
+                                            Text("asdfsdafsdafssadfs", Modifier.padding(10.dp, 5.dp))
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
